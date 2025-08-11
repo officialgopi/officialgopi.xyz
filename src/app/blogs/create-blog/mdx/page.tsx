5e0964fe9b8159bc24f88007fdeda15836bd680b
OfficialGopi <official.gopi@outlook.com>
2025-08-11 10:38:53 +0530

    feat: Add blog pages with basic structure and create blog form

---
+++ src/app/blogs/create-blog/mdx/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full">
+      <form className="w-[600px] h-[600px] border rounded-md border-neutral-500/50 "></form>
+    </div>
+  );
+};
+
+export default page;
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function page() {
-  return <div></div>;
+  return (
+    <section className="flex flex-col w-full">
+      <h1 className="text-4xl font-bold">Blogs</h1>
+      <div className="flex flex-col w-full p-4"></div>
+    </section>
+  );
 }
